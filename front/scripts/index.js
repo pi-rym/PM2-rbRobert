105ba92721eaf903f965f4c9f9eec5fafcd06925
rbRobert <rbalcazar2020.1@gmail.com>
2024-03-11 23:02:51 -0500

    agregando asincronismo con axios

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,5 +1,33 @@
 const renderCards = require('./renderCards');
+const axios = require("axios");
 
-$.get('https://students-api.2.us-1.fl0.io/movies',(data) => {
-  data.forEach(renderCards)
-})
+// const fetchData = async() => {
+//   try {
+//     const data = await axios.get('https://students-api.up.railway.app/movies');
+//     console.log(data.data); 
+//   } catch (error) {
+//     throw new error("No se encontro la API get", error.message);  
+//     console.log(error);  
+//   }
+// }
+// fetchData()
+
+axios.get('https://students-api.up.railway.app/movies')
+     .then((response) => {
+       console.log("Respuesta exitosa.", response);
+
+       response.data.forEach( pelicula => {
+         renderCards(pelicula);
+       })
+     })
+     .catch((error) => {
+       console.error("Error al realizar la peticion.", error.message);
+     });
+
+
+// * anterior
+// $.get('https://students-api.up.railway.app/movies',(data, status) => {
+//   data.forEach(renderCards)
+// })
+
+// ! final 
