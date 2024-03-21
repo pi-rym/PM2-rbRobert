3b465abe9e1f90096e434351d54d34f0eeb684b6
rbRobert <rbalcazar2020.1@gmail.com>
2024-03-21 01:47:29 -0500

    actualizando 01

--- front/scripts/createMovie.js
+++ front/scripts/createMovie.js
@@ -3,10 +3,8 @@ const axios = require("axios");
 (function () {
     "use strict";
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll(".needs-validation");
   
-    // Loop over them and prevent submission
     Array.prototype.slice.call(forms).forEach(function (form) {
       form.addEventListener("submit", function (event) {
           if (!form.checkValidity()) {
